874efc926e3f63af2b25f4d02f45f8becb1dee1d
Daniel <escotodanny62@gmail.com>
2021-09-27 21:38:33 -0700

    refactor for small tablets

--- js/index.js
+++ js/index.js
@@ -9,3 +9,47 @@ navButton.addEventListener("click", (e) => {
     dropdown.classList.add("is-hidden");
   }
 });
+
+// sticky nav
+window.onscroll = () => {
+  modifyNav();
+  modifyHighlight();
+};
+
+const navbar = document.querySelector(".site-nav");
+const sticky = navbar.offsetTop;
+
+const modifyNav = () => {
+  if (window.scrollY > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
+// change highlight
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
+
+const modifyHighlight = () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (window.scrollY === 0) {
+      current = "welcome";
+    }
+    if (window.scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+
+    if (li.textContent.toLowerCase() === current) {
+      li.classList.add("active");
+    }
+  });
+};
